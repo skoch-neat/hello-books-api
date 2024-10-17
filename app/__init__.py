ffda804f484c7913cc77ccf2d69bbbf652e3c63a
Sarah Koch <sarahkoch810@gmail.com>
2024-10-16 22:17:21 -0700

    Registered first Blueprint with app.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from .routes.hello_world_routes import hello_world_bp
 
 def create_app():
     app = Flask(__name__)
+
+    app.register_blueprint(hello_world_bp)
 
     return app
