71ef9dbfabd8e87efd80e5cf0b1b4da8630d7228
Sarah Koch <sarahkoch810@gmail.com>
2024-10-21 00:03:01 -0700

    Refactored error handling to a helper function

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.book import books
 
 books_bp = Blueprint('books_bp', __name__, url_prefix='/books')
@@ -14,21 +14,27 @@ def get_all_books():
                 'description': book.description
             }
         )
-    return books_response
 
 @books_bp.get('/<book_id>')
-def get_one_book(book_id):
+def get_one_book(book_id):    
+    book = validate_book(book_id)
+    
+    return {
+        'id': book.id,
+        'title': book.title,
+        'description': book.description
+    }
+
+def validate_book(book_id):
     try:
         book_id = int(book_id)
     except ValueError:
-        return {"message": f"book {book_id} invalid"}, 400
-    
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
+
     for book in books:
         if book.id == book_id:
-            return {
-                'id': book.id,
-                'title': book.title,
-                'description': book.description
-            }
-        
-    return {"message": f"book {book_id} not found"}, 404
+            return book
+            
+    response = {"message": f"book {book_id} not found"}
+    abort(make_response(response, 404))
