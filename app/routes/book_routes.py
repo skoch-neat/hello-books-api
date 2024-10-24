48cd7643db7839990d454050d45a14e47510643f
Sarah Koch <sarahkoch810@gmail.com>
2024-10-23 23:27:28 -0700

    Removed hardcoded data

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,30 +1,3 @@
-from flask import Blueprint, abort, make_response
-from app.models.book import books
+from flask import Blueprint
 
 books_bp = Blueprint('books_bp', __name__, url_prefix='/books')
-
-@books_bp.get('')
-def get_all_books():
-    books_response = []
-    for book in books:
-        books_response.append(book.to_dict())
-
-@books_bp.get('/<book_id>')
-def get_one_book(book_id):    
-    book = validate_book(book_id)
-    
-    return book.to_dict(), 200
-
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except ValueError:
-        response = {"message": f"book {book_id} invalid"}
-        abort(make_response(response, 400))
-
-    for book in books:
-        if book.id == book_id:
-            return book
-            
-    response = {"message": f"book {book_id} not found"}
-    abort(make_response(response, 404))
