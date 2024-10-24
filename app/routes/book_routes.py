d4e34eecf0ad1b4e26f155b415eaed6e0368c813
Sarah Koch <sarahkoch810@gmail.com>
2024-10-24 01:07:33 -0700

    Created a GET request to the /books path for all books.

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -20,3 +20,19 @@ def create_book():
         'description': new_book.description,
     }
     return response, 201
+
+@books_bp.get('')
+def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return books_response
