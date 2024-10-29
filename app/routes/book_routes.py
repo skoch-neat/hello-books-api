a1cf7b5a82741f7650f95bf4ea0731197dd76aaf
Sarah Koch <sarahkoch810@gmail.com>
2024-10-28 23:24:42 -0700

    Adds ability to filter by query params. Functionality to update and delete book entries added. Updated logic in id validation.

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.db import db
 from app.models.book import Book
 
@@ -19,14 +19,46 @@ def create_book():
 
 @books_bp.get('')
 def get_all_books():
-    query = db.select(Book).order_by(Book.id)
-    books = db.session.scalars(query)
+    query = db.select(Book)
 
-    return [book.to_dict() for book in books]
+    title_param = request.args.get('title')
+    if title_param:
+        query = query.where(Book.title.ilike(f'%{title_param}%'))
+
+    description_param = request.args.get('description')
+    if description_param:
+        query = query.where(Book.description.ilike(f'%{description_param}%'))
+
+    books = db.session.scalars(query.order_by(Book.id))
+
+    books_response = [book.to_dict() for book in books]
+
+    return books_response
 
 @books_bp.get('/<book_id>')
 def get_one_book(book_id):
     return validate_book(book_id).to_dict()
+
+@books_bp.put('/<book_id>')
+def update_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
+
+    book.title = request_body['title']
+    book.description = request_body['description']
+
+    db.session.commit()
+    
+    return Response(status=204, mimetype='application/json')
+
+@books_bp.delete('/<book_id>')
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return Response(status=204, mimetype='application/json')
 
 def validate_book(book_id):
     try:
@@ -34,8 +66,10 @@ def validate_book(book_id):
     except ValueError:
         abort(make_response({'message': f'book {book_id} invalid'}, 400))
 
-        book = db.session.get(Book, book_id)
-        if book is None:
-            abort(make_response({'message': f'book {book_id} not found'}, 404))
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
+
+    if not book:
+        abort(make_response({'message': f'book {book_id} not found'}, 404))
 
     return book
