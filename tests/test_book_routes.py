ec6c27c102e2f3d40694065892424e180b936f06
Sarah Koch <sarahkoch810@gmail.com>
2024-10-30 21:27:15 -0700

    Added testing to successfully create a book.

--- tests/test_book_routes.py
+++ tests/test_book_routes.py
@@ -19,3 +19,24 @@ def test_get_one_book_succeeds(client, two_saved_books):
         "title": "Ocean Book",
         "description": "watr 4evr"
     }
+
+def test_create_one_book(client):
+    # Act
+    response = client.post("/books", json={
+        "title": "New Book",
+        "description": "The Best!"
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 1,
+        "title": "New Book",
+        "description": "The Best!"
+    }
+
+# GET /planets/1 returns a response body that matches our fixture
+# GET /planets/1 with no data in test database (no fixture) returns a 404
+# GET /planets with valid test data (fixtures) returns a 200 with an array including appropriate test data
+# POST /planets with a JSON request body returns a 201
